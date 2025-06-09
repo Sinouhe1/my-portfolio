c661f78131fe1c80ec662b4638ece0643947a210
Sinouhé Ferreira <sinouhe.monteironunes@gmail.com>
2025-06-09 22:56:44 +0200

    add working hamburger menu functionality, added click functionality

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const hamburger = document.getElementById('hamburger-btn');
 const navigationMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', function () {
+  navigationMenu.classList.toggle('show');
+});
