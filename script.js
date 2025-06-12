1dadf6272ec7ca3f5e3e67c17653410d3cd77779
Sinouhé Ferreira <sinouhe.monteironunes@gmail.com>
2025-06-12 22:08:45 +0200

    fix nav-bar open menu on mobile and resizes the screen on desktop width

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const navigationMenu = document.getElementById('nav-menu');
 hamburger.addEventListener('click', function () {
   navigationMenu.classList.toggle('show');
 });
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth > 768) {
+    navigationMenu.classList.remove('show');
+  }
+});
+
+document.querySelectorAll('#nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navigationMenu.classList.remove('show');
+  });
+});
